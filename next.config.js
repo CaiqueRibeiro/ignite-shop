c9013c08c72fc5425e7325adf24b009d8d3c6c09
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2023-07-09 00:18:08 -0300

    feature: main features done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'files.stripe.com'
+    ]
+  }
+}
 
 module.exports = nextConfig
---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { globalStyles } from "@/styles/global"
+import { AppProps } from "next/app"
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "@/styles/pages/app"
+import Image from "next/image"
+
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+        <Header>
+          <Image src={logoImg} alt="" />
+        </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
+export default App
